cbba4b8fe4c64632f205e2ace39974681fe5f84c
vsyx <vsyxio@gmail.com>
2020-12-31 02:18:20 +0200

    Add dictionary for headers

--- src/YoutubeApi/YoutubeApiConfig.cs
+++ src/YoutubeApi/YoutubeApiConfig.cs
@@ -1,22 +1,26 @@
 using System.Net.Http;
+using System.Collections.Generic;
 
 namespace YoutubeApi
 {
     public class YoutubeApiConfig
     {
         public HttpClient Client { get; private set; }
-        public string YoutubeCookie { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
 
         public YoutubeApiConfig(HttpClient client)
         {
-            this.Client = client;
-            this.YoutubeCookie = "";
+            Client = client;
+            Headers = new Dictionary<string, string>();
         }
 
         public YoutubeApiConfig(HttpClient client, string youtubeCookie)
         {
-            this.Client = client;
-            this.YoutubeCookie = youtubeCookie;
+            Client = client;
+            Headers = new Dictionary<string, string>()
+            {
+                { "Cookie", youtubeCookie }
+            };
         }
     }
 }
