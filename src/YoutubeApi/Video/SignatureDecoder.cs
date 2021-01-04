059fc9fdf3186436aaf64c7dadeb457d8fd003bc
vsyx <vsyxio@gmail.com>
2021-01-04 22:33:37 +0200

    Convert loop to linq aggregagate

--- src/YoutubeApi/Video/SignatureDecoder.cs
+++ src/YoutubeApi/Video/SignatureDecoder.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Web;
+using System.Linq;
 using System.Collections.Generic;
 using YoutubeApi.Video.Models;
 
@@ -18,16 +19,13 @@ namespace YoutubeApi.Video
 
         public string Decode(string signature, bool isUrlDecoded = false)
         {
-            var sig = (isUrlDecoded) ? signature : HttpUtility.UrlDecode(signature);
+            var urlDecodedSignature = (isUrlDecoded) ? signature : HttpUtility.UrlDecode(signature);
 
-            var sigArray = sig.ToCharArray();
-
-            foreach (var transformFunction in TransformFunctions)
-            {
-                sigArray = CipherFunctions[transformFunction.Name](sigArray, transformFunction.Arg);
-            }
-
-            return new string(sigArray);
+            return TransformFunctions.Aggregate(
+                    urlDecodedSignature.ToCharArray(), 
+                    (signatureArray, transformFunction) 
+                        => CipherFunctions[transformFunction.Name](signatureArray, transformFunction.Arg),
+                    decipheredSignature => new string(decipheredSignature));
         }
     }
 }
