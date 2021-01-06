149ff72d2024d75dd5a9bb6eaaebd725e8bd4b94
vsyx <vsyxio@gmail.com>
2021-01-07 00:48:09 +0200

    Change videoId matching to a loop

--- src/YoutubeApi/Video/VideoIdExtractor.cs
+++ src/YoutubeApi/Video/VideoIdExtractor.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
-using YoutubeApi.Util;
 using YoutubeApi.Exceptions;
 
 namespace YoutubeApi.Video
@@ -8,21 +8,20 @@ namespace YoutubeApi.Video
     public class VideoIdExtractor
     {
         private const string VideoIdPattern = @"[0-9A-Za-z_-]{10}[048AEIMQUYcgkosw]";
-        private static readonly string[] VideoIdPatterns = {
+        private static readonly Regex[] VideoIdRegexes = new string[] {
             $"^(?:https?://)?(?:www\\.?)?(?:m\\.?)?youtube\\.com/.*?v=({VideoIdPattern})", // regular url
             $"^(?:https?://)?youtu\\.be/({VideoIdPattern})$", // short url
             $"^({VideoIdPattern})$" // just id
-        };
-
-        private static readonly Regex VideoIdRegex = new Regex(RegexUtils.CombinePatterns(VideoIdPatterns));
+        }.Select(pattern => new Regex(pattern)).ToArray();
 
         public static string Extract(string stringContainingId)
         {
-            var match = VideoIdRegex.Match(stringContainingId);
             try
             {
-                var videoIdGroup = RegexUtils.ExtractFirstValidGroupMatch(match.Groups, 1);
-                return videoIdGroup.Value;
+                return VideoIdRegexes
+                    .Select(regex => regex.Match(stringContainingId))
+                    .Where(match => match.Success)
+                    .First().Groups[1].Value;
             }
             catch (InvalidOperationException)
             {
